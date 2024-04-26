2dd67448109553106f36aba9013f20cf60854b33
Romeo Angeles Jr <romeo@cotit.eu>
2024-04-27 04:10:01 +0800

    feat: testimonials section

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <section class="flex flex-col py-20">
-            <h2 class="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+        <section class="flex flex-col my-24 md:mt-36">
+            <h2 class="mt-10 scroll-m-20 pb-2 text-5xl font-semibold tracking-tight transition-colors first:mt-0">
                 Hello!
             </h2>
-            <h1 class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+            <h1 class="scroll-m-20 text-5xl font-bold tracking-tight lg:text-7xl">
                 You can call me <span
                     class="bg-gradient-to-r from-primary to-fuchsia-500 bg-clip-text text-transparent">Romeo</span>
             </h1>
-            <p class="leading-7 mt-3">
+            <p class="leading-7 mt-3 text-xl">
                 Front-end Engineer at <a href="#"
                     class="bg-gradient-to-r from-primary to-fuchsia-500 bg-clip-text text-transparent">
                     Samsung
@@ -58,6 +58,51 @@
                         </TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
+            </div>
+        </section>
+        <section class="flex flex-col py-20">
+            <div class="flex items-center">
+                <div class="flex flex-col mt-5 items-center md:mx-auto overflow-clip">
+                    <h2
+                        class="scroll-m-20 text-xl font-semibold tracking-wide transition-colors first:mt-0 text-primary">
+                        Colleagues
+                    </h2>
+                    <h2 class="scroll-m-20 text-4xl font-semibold tracking-wide transition-colors first:mt-0">
+                        Thoughts about me
+                    </h2>
+                    <div class="flex p-1 space-x-5 mt-10 ">
+                        <Card v-for="(_, index) in 5">
+                            <CardContent class="flex space-x-10 p-6 w-[700px]">
+                                <div class="flex flex-col justify-center flex-[2] items-center">
+                                    <Avatar class="mb-3">
+                                        <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <small>John Doe</small>
+                                    <small><b>Front End Engineer</b></small>
+                                </div>
+                                <div class="flex flex-col justify-center flex-[8]">
+                                    <i>
+                                        <small>
+                                            &ldquo;John is a talented front-end engineer with a keen eye for
+                                            detail and a
+                                            deep understanding of HTML, CSS, and JavaScript. He consistently
+                                            delivers high
+                                            quality work and is always eager to learn and improve. He is a
+                                            team
+                                            player,
+                                            communicates effectively, and is always open to feedback and
+                                            constructive
+                                            criticism. I highly recommend John for any front-end related
+                                            hiring
+                                            needs.&rdquo;
+                                        </small>
+                                    </i>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
